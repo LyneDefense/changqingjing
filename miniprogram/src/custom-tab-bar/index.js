a31725bd22835c1701c62074fa5eab678f65df7c
hupinjie <pinjhu@hupinjiedeMacBook-Pro.local>
2026-09-10 22:40:32 +0800

    简化小程序底部导航

--- miniprogram/src/custom-tab-bar/index.js
+++ miniprogram/src/custom-tab-bar/index.js
@@ -4,27 +4,19 @@ Component({
     tabs: [
       {
         pagePath: '/pages/index/index',
-        text: '首页',
-        iconPath: '../assets/tabbar/home.png',
-        selectedIconPath: '../assets/tabbar/home-selected.png'
+        text: '首页'
       },
       {
         pagePath: '/pages/member/index',
-        text: '会员专区',
-        iconPath: '../assets/tabbar/member.png',
-        selectedIconPath: '../assets/tabbar/member-selected.png'
+        text: '会员专区'
       },
       {
         pagePath: '/pages/cooperation/index',
-        text: '合作权益',
-        iconPath: '../assets/tabbar/cooperation.png',
-        selectedIconPath: '../assets/tabbar/cooperation-selected.png'
+        text: '合作权益'
       },
       {
         pagePath: '/pages/profile/index',
-        text: '个人中心',
-        iconPath: '../assets/tabbar/profile.png',
-        selectedIconPath: '../assets/tabbar/profile-selected.png'
+        text: '个人中心'
       }
     ]
   },
